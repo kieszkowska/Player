5d15501bb8797946b2494c8a73fecec91045372a
Klaudia Kieszkowska <claudiaandjazz@gmail.com>
2018-08-14 09:53:10 +0200

    More list styles

--- src/components/list/SongsList.js
+++ src/components/list/SongsList.js
@@ -7,22 +7,25 @@ class SongsList extends Component {
 
     render() {
         return (
-            <div className='songsList'>
-                { this.props.songs.map((el, i) => {
-                    return (
-                        <div className='listElement' key={ i } onClick={ () => this.props.songHandler(i) }>
-                            <Link to={ '/' }>
-                                <span className='secondaryText'>{ el.time } | { el.artist }</span>
-                                <br />
-                                <span className='title'>{ el.title }</span>
-                            </Link>
-                            <div key={ i * 100 } >
-                                <img src={ share } alt='Share' />
-                                <img src={ favorite } alt='Add to favorite' />
+            <div className='songsContainer'>
+                <div className='songsList'>
+                    { this.props.songs.map((el, i) => {
+                        return (
+                            <div className='listElement' key={ i } onClick={ () => this.props.songHandler(i) }>
+                                <Link to={ '/' }>
+                                    <span className='secondaryText'>{ el.time } | { el.artist }</span>
+                                    <br />
+                                    <span className='title'>{ el.title }</span>
+                                </Link>
+                                <div key={ i * 100 } >
+                                    <img className='btn-transparent' src={ share } alt='Share' />
+                                    <img className='btn-transparent' src={ favorite } alt='Add to favorite' />
+                                </div>
                             </div>
-                        </div>
-                    );
-                }) }
+                        );
+                    }) }
+                </div>
+                <div className='shadow'>{ null }</div>
             </div>
         );
     }
